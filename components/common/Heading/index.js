f42b9cd914d01e20a9506073fc46d54de319c0d5
Louis Weiser <louis.weiser98@gmail.com>
2023-03-19 12:24:43 +0100

    update

--- components/common/Heading/index.js
+++ components/common/Heading/index.js
@@ -5,9 +5,6 @@ import styles from "./heading.module.css";
 export default function Header({ title }) {
   return (
     <header>
-      <Head>
-        <title>{title}</title>
-      </Head>
       <h1 className={title === "BookLog" ? styles.headerhome : styles.header}>
         {title}
       </h1>
