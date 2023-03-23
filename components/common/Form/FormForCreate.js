e2d24d259b8b9fdd93cbd28a25e194080f2b4c91
Louis Weiser <louis.weiser98@gmail.com>
2023-03-23 20:32:24 +0100

    update

--- components/common/Form/FormForCreate.js
+++ components/common/Form/FormForCreate.js
@@ -1,9 +1,61 @@
 import { useState } from "react";
 
+import { genreData } from "@/public/data/genre.js";
+
+import styles from "./form.module.css";
+
+const GenreCheckbox = ({ genre, checked, onGenreChange }) => {
+  const handleCheckboxChange = (event) => {
+    onGenreChange(genre, event.target.checked);
+  };
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        value={genre}
+        checked={checked}
+        onChange={handleCheckboxChange}
+      />
+      {genre}
+    </label>
+  );
+};
+
+const GenreDropdown = ({ genres, onGenreChange }) => {
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
+  const handleGenreChange = (genre, checked) => {
+    let newSelectedGenres;
+    if (checked) {
+      newSelectedGenres = [...selectedGenres, genre];
+    } else {
+      newSelectedGenres = selectedGenres.filter((g) => g !== genre);
+    }
+    setSelectedGenres(newSelectedGenres);
+    onGenreChange(newSelectedGenres);
+  };
+
+  return (
+    <div>
+      {genres.map((genre) => (
+        <GenreCheckbox
+          key={genre}
+          genre={genre}
+          checked={selectedGenres.includes(genre)}
+          onGenreChange={handleGenreChange}
+        />
+      ))}
+    </div>
+  );
+};
+
 export default function FormForCreate() {
   const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+
   const [image, setImage] = useState("");
-  const [genre, setGenre] = useState("");
+  const [genre, setGenre] = useState([]);
   const [author, setAuthor] = useState("");
 
   const handleSubmit = (event) => {
@@ -13,39 +65,65 @@ export default function FormForCreate() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        title:
+      <label htmlFor="title">
         <input
+          className={styles.input1}
+          placeholder="Title"
           type="text"
+          id="title"
+          maxLength="50"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
       </label>
-      <br />
-      <label>
-        cover:
-        <input type="file" onChange={(e) => setImage(e.target.files[0])} />
-      </label>
-      <br />
-      <label>
-        author:
+      <label htmlFor="subtitle">
         <input
+          className={styles.input1}
+          placeholder="Subtitle"
           type="text"
+          id="subtitle"
+          maxLength="50"
+          value={subtitle}
+          onChange={(e) => setSubtitle(e.target.value)}
+        />
+      </label>
+      <label htmlFor="author">
+        <input
+          className={styles.input1}
+          placeholder="Author"
+          type="text"
+          id="author"
+          maxLength="50"
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         />
       </label>
-      <br />
-      <label>
+      <GenreDropdown genres={genreData} onGenreChange={setGenre} />
+      <label htmlFor="genre">
+        <select
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+          multiple
+        >
+          {genreData.map((genre) => (
+            <option key={genre} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
+      </label>
+      {/*       <label>
         genre:
         <select value={genre} onChange={(e) => setGenre(e.target.value)}>
-          <option value="">Bitte auswählen</option>
+          <option value="">Genre Auswählen</option>
           <option value="Romance">Romance</option>
           <option value="Mystery">Mystery</option>
           <option value="Fantasy">Fantasy</option>
         </select>
-      </label>
-      <br />
+      </label> */}
+
+      <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+
       <button type="submit">add book</button>
     </form>
   );
