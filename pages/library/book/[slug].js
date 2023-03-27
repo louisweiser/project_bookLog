032c63dd76afbc1aea34bc16d4c83ac67f5c4809
Louis Weiser <louis.weiser98@gmail.com>
2023-03-27 23:29:01 +0200

    update

--- pages/library/book/[slug].js
+++ pages/library/book/[slug].js
@@ -1,91 +1,46 @@
 import { useRouter } from "next/router";
-
 import { useContext } from "react";
-import { DataContext } from "@/contexts/dataContext.js";
-
 import Background from "@/components/common/Background/blue.js";
 import Header from "@/components/common/Heading/Detail.js";
 import CoverFromData from "@/components/common/Cover/coverData.js";
-
-import { bookMetaData } from "@/public/data/bookmeta.js";
-import { bookSummary } from "@/public/data/bookentries/booksummary.js";
-import { bookStories } from "@/public/data/bookentries/bookstory.js";
-
+import { DataContext } from "@/contexts/dataContext.js";
 import styles from "./slug.module.css";
 
-function BookDetail() {
-  const router = useRouter();
-
-  const { bookData } = useContext(DataContext);
-
-  const index = bookData.findIndex((book) => book.slug === router.query.slug);
-
-  //const summary = bookSummary[result.id - 1];
-  //const story1 = bookStories[result.id - 1]?.[0];
-  //const story2 = bookStories[result.id - 1]?.[1];
-
+function BookDetail({ book }) {
   return (
     <>
       <Background></Background>
       <Header></Header>
       <div className={styles.body}>
         <div className={styles.cover}>
-          <CoverFromData
-            slug={bookData[index].slug}
-            height={300}
-          ></CoverFromData>
+          <CoverFromData slug={book.slug} height={300}></CoverFromData>
         </div>
-        {/* {summary && (
-          <div className={styles.summary}>
-            <h3>Summary :</h3>
-            <h4>{summary.summary}</h4>
-          </div>
-        )}
-        {story1 && (
-          <div className={styles.summary}>
-            <h3>Storie 1: {story1.title}</h3>
-            <h4>{story1.text}</h4>
-          </div>
-        )}
-        {story2 && (
-          <div className={styles.summary}>
-            <h3>Storie 2: {story2.title}</h3>
-            <h4>{story2.text}</h4>
-          </div>
-        )} */}
       </div>
     </>
   );
 }
 
+export default BookDetail;
+
+// getServerSideProps
 export async function getServerSideProps(context) {
-  const { slug } = context.params;
+  const slug = context.params.slug;
 
-  function searchArray(array, searchTerm) {
-    for (let i = 0; i < array.length; i++) {
-      const object = array[i];
+  // Lade Buchdaten über die API
+  const response = await fetch("http://localhost:3000/api/books");
+  const bookData = await response.json();
 
-      for (const key in object) {
-        if (object.hasOwnProperty(key)) {
-          const value = object[key];
+  const index = bookData.findIndex((book) => book.slug === slug);
 
-          if (value === searchTerm) {
-            return object;
-          }
-        }
-      }
-    }
-
-    return null;
+  if (index === -1) {
+    return {
+      notFound: true,
+    };
   }
-
-  const result = searchArray(bookMetaData, slug);
 
   return {
     props: {
-      result,
+      book: bookData[index],
     },
   };
 }
-
-export default BookDetail;
